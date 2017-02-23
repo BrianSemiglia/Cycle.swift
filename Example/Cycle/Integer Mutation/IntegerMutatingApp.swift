c9bdcdabfd3e0b1d678af38e54e4be2588b0a89c
Brian Semiglia <brian.semiglia@gmail.com>
2017-02-23 00:00:34 -0500

    Adds Integer Mutation example. Adds missing dependencies.

---
+++ Example/Cycle/Integer Mutation/IntegerMutatingApp.swift
@@ -0,0 +1,88 @@
+//
+//  Example.swift
+//  Cycle
+//
+//  Created by Brian Semiglia on 1/20/17.
+//  Copyright © 2017 Brian Semiglia. All rights reserved.
+//
+
+import Foundation
+import Cycle
+import RxSwift
+
+@UIApplicationMain
+class Example: CycledApplicationDelegate<IntegerMutatingApp> {
+  init() {
+    super.init(
+      filter: IntegerMutatingApp()
+    )
+  }
+}
+
+struct IntegerMutatingApp: SinkSourceConverting {
+  struct Model: Initializable {
+    var screen = ValueToggler.Model.empty
+    var application = RxUIApplication.Model.empty
+  }
+  struct Drivers: CycleDrivable {
+    let toggler = ValueToggler()
+    var application: RxUIApplication!
+  }
+  func effectsFrom(events: Observable<Model>, drivers: Drivers) -> Observable<Model> {
+    let value = drivers.toggler
+      .rendered(events.map { $0.screen })
+      .withLatestFrom(events) { ($0.0, $0.1) }
+      .reduced()
+    
+    let application = drivers.application
+      .rendered(events.map { $0.application })
+      .withLatestFrom(events) { ($0.0, $0.1) }
+      .reduced()
+    
+    return Observable.of(value, application).merge()
+  }
+}
+
+extension IntegerMutatingApp.Model {
+  static var empty: IntegerMutatingApp.Model { return
+    IntegerMutatingApp.Model(
+      screen: .empty,
+      application: .empty
+    )
+  }
+}
+
+extension ObservableType where E == (ValueToggler.Model, IntegerMutatingApp.Model) {
+  func reduced() -> Observable<IntegerMutatingApp.Model> { return
+    map { event, global in
+      var x = global
+      x.screen = event
+      if event.increment.state == .highlighted {
+        x.screen.total = Int(x.screen.total).map { $0 + 1 }.map(String.init) ?? ""
+        x.screen.increment.state = .enabled
+      }
+      if event.decrement.state == .highlighted {
+        x.screen.total = Int(x.screen.total).map { $0 - 1 }.map(String.init) ?? ""
+        x.screen.decrement.state = .enabled
+      }
+      return x
+    }
+  }
+}
+
+extension ObservableType where E == (RxUIApplication.Model, IntegerMutatingApp.Model) {
+  func reduced() -> Observable<IntegerMutatingApp.Model> { return
+    map { event, global in
+      var c = global
+      var model = event
+      model.shouldLaunch = true
+      c.application = model
+      var s = c.screen
+      if case .pre(.active(.some)) = event.session.state {
+        s.total = "55"
+      }
+      c.screen = s
+      return c
+    }
+  }
+}
