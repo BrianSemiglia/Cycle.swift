963e6886b8f3ee320b16afb4c3265a3832a775d7
Brian Semiglia <brian.semiglia@gmail.com>
2018-09-16 18:10:21 -0400

    Restructures project for multiplatform support.

---
+++ Cycle.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+
+import Cycle
+
+var str = "Hello, playground"
---
+++ Sources/Cycle.h
@@ -0,0 +1,17 @@
+//
+//  Cycle.h
+//  Cycle
+//
+//  Created by Brian Semiglia on 04/10/16.
+//  Copyright © 2017 BrianSemiglia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Cycle.
+FOUNDATION_EXPORT double CycleVersionNumber;
+
+//! Project version string for Cycle.
+FOUNDATION_EXPORT const unsigned char CycleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cycle/PublicHeader.h>
