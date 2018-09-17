429d79f1f87ca64e4f4f2e51929dfa57f0df9e10
Brian Semiglia <brian.semiglia@gmail.com>
2018-09-17 13:38:21 -0400

    Adds cycle for watchOS.

--- Sources/Core/Cycle.swift
+++ Sources/Core/Cycle.swift
@@ -128,6 +128,108 @@ public protocol UIApplicationDelegateProviding {
   var application: Delegate { get }
 }
 
+#elseif os(watchOS)
+
+import WatchKit
+import RxSwift
+
+open class CycledApplicationDelegate<T: IORouter>: NSObject, WKExtensionDelegate {
+  
+  private var cycle: Cycle<T>
+  
+  public override init() {
+    fatalError("CycledApplicationDelegate must be instantiated with a router.")
+  }
+  
+  public init(router: T) {
+    cycle = Cycle(router: router)
+    super.init()
+  }
+  
+  override open func forwardingTarget(for input: Selector!) -> Any? { return
+    cycle.delegate
+  }
+  
+  override open func responds(to input: Selector!) -> Bool { return
+    cycle.delegate.responds(to: input)
+  }
+}
+
+public final class Cycle<E: IORouter> {
+  private var output: Observable<E.Frame>?
+  private var inputProxy: ReplaySubject<E.Frame>?
+  private let cleanup = DisposeBag()
+  private let drivers: E.Drivers
+  fileprivate let delegate: WKExtensionDelegate
+  fileprivate let root: WKInterfaceController
+  public required init(router: E) {
+    inputProxy = ReplaySubject.create(
+      bufferSize: 1
+    )
+    drivers = router.driversFrom(seed: E.seed)
+    root = drivers.screen.root
+    delegate = drivers.application
+    output = router.effectsOfEventsCapturedAfterRendering(
+      incoming: inputProxy!,
+      to: drivers
+    )
+    // `.startWith` is redundant, but necessary to kickoff cycle
+    // Possibly removed if `output` was BehaviorSubject?
+    // Not sure how to `merge` observables to single BehaviorSubject though.
+    output?
+      .startWith(E.seed)
+      .subscribe(self.inputProxy!.on)
+      .disposed(by: cleanup)
+  }
+}
+
+public protocol IORouter {
+  
+  /*
+   Defines schema and initial values of application model.
+   */
+  associatedtype Frame
+  static var seed: Frame { get }
+  
+  /*
+   Defines drivers that handle effects, produce events. Requires two default drivers:
+   
+   1. let application: UIApplicationDelegateProviding - can serve as UIApplicationDelegate
+   2. let screen: ScreenDrivable - can provide a root UIViewController
+   
+   A default UIApplicationDelegateProviding driver, RxUIApplicationDelegate, is included with Cycle.
+   */
+  associatedtype Drivers: UIApplicationDelegateProviding, ScreenDrivable
+  
+  /*
+   Instantiates drivers with initial model. Necessary to for drivers that require initial values.
+   */
+  func driversFrom(seed: Frame) -> Drivers
+  
+  /*
+   Returns a stream of Models created by rendering the incoming stream of effects to Drivers and then capturing and transforming Driver events into the Model type.
+   */
+  func effectsOfEventsCapturedAfterRendering(
+    incoming: Observable<Frame>,
+    to drivers: Drivers
+    ) -> Observable<Frame>
+  
+}
+
+public protocol ScreenDrivable {
+  associatedtype Driver: UIViewControllerProviding
+  var screen: Driver { get }
+}
+
+public protocol UIViewControllerProviding {
+  var root: WKInterfaceController { get }
+}
+
+public protocol UIApplicationDelegateProviding {
+  associatedtype Delegate: WKExtensionDelegate
+  var application: Delegate { get }
+}
+
 #elseif os(macOS)
 
 import AppKit
