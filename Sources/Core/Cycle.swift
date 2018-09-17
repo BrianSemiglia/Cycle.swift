cf7d9d213e21936eebf48e94431cc557ff016272
Brian Semiglia <brian.semiglia@gmail.com>
2018-09-16 23:07:52 -0400

    Adds Cycle for macOS.

--- Sources/Core/Cycle.swift
+++ Sources/Core/Cycle.swift
@@ -128,4 +128,132 @@ public protocol UIApplicationDelegateProviding {
   var application: Delegate { get }
 }
 
+#elseif os(macOS)
+
+import AppKit
+import RxSwift
+
+open class CycledApplicationDelegate<T: IORouter>: NSObject, NSApplicationDelegate {
+  
+  private var cycle: Cycle<T>
+  public var main: NSWindowController? // <-- change to (NSWindow, NSViewController) combo to avoid internal storyboard use below
+  
+  public override init() {
+    fatalError("CycledApplicationDelegate must be instantiated with a router.")
+  }
+
+  public init(router: T) {
+    cycle = Cycle(router: router)
+    super.init()
+  }
+
+  public func applicationWillFinishLaunching(_ notification: Notification) {
+    main = NSStoryboard(
+      name: NSStoryboard.Name(rawValue: "Main"),
+      bundle: nil
+    )
+    .instantiateController(
+      withIdentifier: NSStoryboard.SceneIdentifier(rawValue: "MainWindow")
+    )
+    as? NSWindowController
+    main?.window?.contentViewController = cycle.root
+    main?.window?.makeKeyAndOrderFront(nil)
+  }
+  
+  override open func forwardingTarget(for input: Selector!) -> Any? { return
+    cycle.delegate
+  }
+  
+  override open func responds(to input: Selector!) -> Bool {
+    if input == #selector(applicationWillFinishLaunching(_:)) {
+      applicationWillFinishLaunching(
+        Notification(
+          name: Notification.Name(
+            rawValue: ""
+          )
+        )
+      )
+    }
+    return cycle.delegate.responds(
+      to: input
+    )
+  }
+  
+}
+
+public final class Cycle<E: IORouter> {
+  private var output: Observable<E.Frame>?
+  private var inputProxy: ReplaySubject<E.Frame>?
+  private let cleanup = DisposeBag()
+  private let drivers: E.Drivers
+  fileprivate let delegate: NSApplicationDelegate
+  fileprivate let root: NSViewController
+  public required init(router: E) {
+    inputProxy = ReplaySubject.create(
+      bufferSize: 1
+    )
+    drivers = router.driversFrom(seed: E.seed)
+    root = drivers.screen.root
+    delegate = drivers.application
+    output = router.effectsOfEventsCapturedAfterRendering(
+      incoming: inputProxy!,
+      to: drivers
+    )
+    // `.startWith` is redundant, but necessary to kickoff cycle
+    // Possibly removed if `output` was BehaviorSubject?
+    // Not sure how to `merge` observables to single BehaviorSubject though.
+    output?
+      .startWith(E.seed)
+      .subscribe(self.inputProxy!.on)
+      .disposed(by: cleanup)
+  }
+}
+
+public protocol IORouter {
+
+  /*
+   Defines schema and initial values of application model.
+   */
+  associatedtype Frame
+  static var seed: Frame { get }
+
+  /*
+   Defines drivers that handle effects, produce events. Requires two default drivers:
+
+   1. let application: UIApplicationDelegateProviding - can serve as UIApplicationDelegate
+   2. let screen: ScreenDrivable - can provide a root UIViewController
+
+   A default UIApplicationDelegateProviding driver, RxUIApplicationDelegate, is included with Cycle.
+   */
+  associatedtype Drivers: UIApplicationDelegateProviding, ScreenDrivable
+
+  /*
+   Instantiates drivers with initial model. Necessary to for drivers that require initial values.
+   */
+  func driversFrom(seed: Frame) -> Drivers
+
+  /*
+   Returns a stream of Models created by rendering the incoming stream of effects to Drivers and then capturing and transforming Driver events into the Model type.
+   */
+  func effectsOfEventsCapturedAfterRendering(
+    incoming: Observable<Frame>,
+    to drivers: Drivers
+    ) -> Observable<Frame>
+
+}
+
+public protocol ScreenDrivable {
+  associatedtype Driver: UIViewControllerProviding
+  var screen: Driver { get }
+}
+
+public protocol UIViewControllerProviding {
+  var root: NSViewController { get }
+}
+
+public protocol UIApplicationDelegateProviding {
+  associatedtype Delegate: NSApplicationDelegate
+  var application: Delegate { get }
+}
+
 #endif
